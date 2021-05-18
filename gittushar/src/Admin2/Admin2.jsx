ca6d0a118ae442a2def1d5884d3f04a1fde55f55
Tushar <tusharsharmalife@gmail.com>
2021-05-18 14:56:27 +0530

    tushar

--- gittushar/src/Admin2/Admin2.jsx
+++ gittushar/src/Admin2/Admin2.jsx
@@ -7,26 +7,76 @@ class Admin2 extends Component {
     render() {
         return (
             <div>
+                <div className="allcard">
+                <div>
                 <Grid container>
                 <Grid item md={1}>
 
                     </Grid>
                     <Grid item md={3}>
-<Card>
+<Card className="fixcard1 marg">
 hello
 </Card>
                     </Grid>
                     <Grid item md={5}>
-<Card>
+<Card className="fixcard">
 tushar
 </Card>
                     </Grid>
                     <Grid item md={3}>
-<Card>
+<Card className="fixcard2 ">
 sharma
 </Card>
                     </Grid>
                 </Grid>
+                </div>
+                <div>
+
+                <Grid container>
+                <Grid item md={1}>
+
+                    </Grid>
+                    <Grid item md={3}>
+<Card className="fixcard1 marg">
+hello
+</Card>
+                    </Grid>
+                    <Grid item md={5}>
+<Card className="fixcard">
+tushar
+</Card>
+                    </Grid>
+                    <Grid item md={3}>
+<Card className="fixcard2">
+sharma
+</Card>
+                    </Grid>
+                </Grid>
+
+                </div>
+                <div>
+                <Grid container>
+                <Grid item md={1}>
+
+                    </Grid>
+                    <Grid item md={3}>
+<Card className="fixcard1 marg">
+hello
+</Card>
+                    </Grid>
+                    <Grid item md={5}>
+<Card className="fixcard">
+tushar
+</Card>
+                    </Grid>
+                    <Grid item md={3}>
+<Card className="fixcard2">
+sharma
+</Card>
+                    </Grid>
+                </Grid>
+                </div>
+                </div>
             </div>
         );
     }
