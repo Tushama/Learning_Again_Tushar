355a3c0408dd8150cd38e62170dc8d5afd5688d0
Your name here <avantikachaudhary99@gmail.com>
2021-05-20 23:01:12 +0530

    Bhawna

--- gittushar/src/Candidate/Candidate.jsx
+++ gittushar/src/Candidate/Candidate.jsx
@@ -1,40 +1,99 @@
 import React, { Component } from "react";
 import HOC from "../HOC";
+import "./Candidate.css";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
+import axios from "axios";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
 export class Candidate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tabledata: [],
+    };
+  }
+  componentDidMount = () => {
+    let url = "http://seo.srcservicesltd.com:8000/resumelist/draft/0";
+
+    axios.get(url).then(
+      (response) => {
+        console.log("thedata", response);
+        this.setState({
+          tabledata: response.data.data,
+        });
+      },
+
+      (error) => {}
+    );
+  };
   render() {
     return (
       <div>
-        <table class="table fortable">
-          <thead class="thead-dark">
-            <tr>
-              <th scope="col">Id</th>
-              <th scope="col">Candidate Name</th>
-              <th scope="col">Last modification</th>
-              <th scope="col">Status</th>
-              <th scope="col">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="space">
+          <h1> Candidate</h1>
+        </div>
+        <div className="displaying">
+          <table class="table displaytable tableoutline">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Candidate Name</th>
+                <th scope="col">Last modification</th>
+                <th scope="col">Status</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.tabledata.map((item, index) => (
+                <tr>
+                  <th>{item.Id}</th>
+                  <th scope="col">
+                    <p>{item.Full_Name}</p>
+                  </th>
+                  <th scope="col">
+                    {new Intl.DateTimeFormat("en-US", {
+                      year: "numeric",
+                      month: "2-digit",
+                      day: "2-digit",
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      
+                    }).format(item.Last_Modifyed)}
+                  </th>
+                  <th scope="col">
+                    <p>{item.resume_status}</p>
+                  </th>
+                  <th scope="col">
+                    <label>
+                      <DeleteIcon
+                        className="deletebutton"
+                        onClick={() => {
+                          confirmAlert({
+                            title:
+                              "Are you sure You won't be able to revert this! ",
+                            buttons: [
+                              {
+                                label: "Yes",
+
+                                onClick: () => alert("Click Yes"),
+                              },
+                              {
+                                label: "No",
+                                onClick: () => alert("Click No"),
+                              },
+                            ],
+                          });
+                        }}
+                      />
+                      <PlayCircleOutlineIcon className="deletebutton" />
+                    </label>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
--- gittushar/src/Recruitertable/RecruiterTable.jsx
+++ gittushar/src/Recruitertable/RecruiterTable.jsx
@@ -2,16 +2,14 @@ import React, { Component } from "react";
 import "./Recruitertable.css";
 import axios from "axios";
 import HOC from "../HOC";
-import DeleteIcon from '@material-ui/icons/Delete';
-import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import DeleteIcon from "@material-ui/icons/Delete";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
 import Swal from "sweetalert2";
 class RecruiterTable extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      alldata: [
-    
-      ],
+      alldata: [],
     };
   }
   componentDidMount = () => {
@@ -28,73 +26,72 @@ class RecruiterTable extends Component {
       (error) => {}
     );
   };
-  delete=()=>{
-  
-Swal.fire({
-  title: 'Are you sure?',
-  text: "You won't be able to revert this!",
-  icon: 'warning',
-  showCancelButton: true,
-  confirmButtonColor: '#3085d6',
-  cancelButtonColor: '#d33',
-  confirmButtonText: 'Yes, delete it!'
-}).then((result) => {
-  if (result.isConfirmed) {
-    Swal.fire(
-      'Deleted!',
-      'Your file has been deleted.',
-      'success'
-    )
-  }
-})
-  }
+  delete = () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
+      }
+    });
+  };
   render() {
     return (
-        <div >
-            <div className="blankspace">
-                <h1>Recruiter</h1>
-            </div>
-            <div className="displaytable1">
-           <table className="table1 table table-striped displaytable tableoutline">
-<thead>
-<tr className="colorback">
-<th scope="row">id</th>
-  <th scope="col">Project Name</th>
-  <th scope="col">Status</th>
-  <th scope="col">Last Modified Date</th>
-  <th scope="col">Action</th>
-</tr>
-</thead>
+      <div>
+        <div className="blankspace">
+          <h1>Recruiter</h1>
+        </div>
+        <div className="displaytable1">
+          <table className="table1 table table-striped displaytable tableoutline">
+            <thead>
+              <tr className="colorback">
+                <th scope="row">id</th>
+                <th scope="col">Project Name</th>
+                <th scope="col">Status</th>
+                <th scope="col">Last Modified Date</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
 
-
-
-
-<tbody>
-{this.state.alldata.map((item,index)=>(
-<tr>
-<th>{item.id}</th>
-<th scope="col"><p>{item.Project_Title}</p></th>
-<th scope="col"><p>{item.Project_Status}</p></th>
-<th scope="col"><p>{item.Last_modification}</p></th>
-<th scope="col"><label><DeleteIcon className="deleteicon" onClick={this.delete}/><PlayCircleOutlineIcon className="deleteicon ml-3" onClick={this.delete}/></label></th >
-</tr>
-
-))}
-
-</tbody>
-</table>
-</div>
+            <tbody>
+              {this.state.alldata.map((item, index) => (
+                <tr>
+                  <th>{item.id}</th>
+                  <th scope="col">
+                    <p>{item.Project_Title}</p>
+                  </th>
+                  <th scope="col">
+                    <p>{item.Project_Status}</p>
+                  </th>
+                  <th scope="col">
+                    <p>{item.Last_modification}</p>
+                  </th>
+                  <th scope="col">
+                    <label>
+                      <DeleteIcon
+                        className="deleteicon"
+                        onClick={this.delete}
+                      />
+                      <PlayCircleOutlineIcon
+                        className="deleteicon ml-3"
+                        onClick={this.delete}
+                      />
+                    </label>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-      
-
-
-     
-        
-
-
-  
     );
-}
+  }
 }
 
 export default HOC(RecruiterTable);
