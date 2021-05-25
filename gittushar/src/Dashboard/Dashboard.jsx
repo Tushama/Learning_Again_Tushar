417fb704ac8a28c11ba424fb1fa4e089890d7e06
Rai Amit <10058186@gemalto.com>
2021-05-25 15:12:36 +0530

    Puja

--- gittushar/src/Dashboard/Dashboard.jsx
+++ gittushar/src/Dashboard/Dashboard.jsx
@@ -1,97 +1,86 @@
-import React, { Component } from 'react';
-import HOC from "../HOC"
-import Card from '@material-ui/core/Card';
-import Grid from '@material-ui/core/Grid';
+import React, { Component } from "react";
+import HOC from "../HOC";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
 import "./Dashboard.css";
 import axios from "axios";
-import RefreshIcon from '@material-ui/icons/Refresh';
-import { Circular } from 'react-graphical-ui';
-import { Histogram } from 'react-graphical-ui';
-import {Bar} from 'react-chartjs-2'
+import RefreshIcon from "@material-ui/icons/Refresh";
+import { Circular } from "react-graphical-ui";
+import { Histogram } from "react-graphical-ui";
+import { Bar } from "react-chartjs-2";
 
 class Dashboard extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-           id:"",
-           Veryfied_user:"",
-           Un_verified_user:"",
-           Total_User:"",
-           Total_Resume:"",
-           Total_Live:"",
-           Total_Deleted_Request:"",
-           Total_Draft:"",
-      
-          }
-     
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      Veryfied_user: "",
+      Un_verified_user: "",
+      Total_User: "",
+      Total_Resume: "",
+      Total_Live: "",
+      Total_Deleted_Request: "",
+      Total_Draft: "",
+    };
+  }
 
-    state = {
-      labels: ['January', 'February', 'March',
-               'April', 'May'],
-      datasets: [
-        {
-          label: 'Rainfall',
-          backgroundColor: 'rgba(75,192,192,1)',
-          borderColor: 'rgba(0,0,0,1)',
-          borderWidth: 2,
-          data: [65, 59, 80, 81, 56]
-        }
-      ]
-    }
+  state = {
+    labels: ["January", "February", "March", "April", "May"],
+    datasets: [
+      {
+        label: "Rainfall",
+        backgroundColor: "rgba(75,192,192,1)",
+        borderColor: "rgba(0,0,0,1)",
+        borderWidth: 2,
+        data: [65, 59, 80, 81, 56],
+      },
+    ],
+  };
 
-    Reload =()=>{
-      let url = "http://seo.srcservicesltd.com:8000/Refresh";
+  Reload = () => {
+    let url = "http://seo.srcservicesltd.com:8000/Refresh";
 
-      axios.patch(url).then(
-        (response) => {
-          console.log("======alldata", response);
-        
-        },
-  
-        (error) => {}
-      );
-    
-      };
-      componentDidMount=()=>{
-        let url =
-        "http://seo.srcservicesltd.com:8000" +
-        "/count_statics" 
-    
-       
-      axios.get(url).then(
-        (response) => { 
-            console.log("======",response)
-          this.setState({
-            id:response.data.id,
-            Veryfied_user:response.data.data.Veryfied_user,
-            Un_verified_user:response.data.data.Un_verified_user,
-            Total_User:response.data.data.Total_User,
-            Total_Resume:response.data.data.Total_Resume,
-            Total_Live:response.data.data.Total_Live,
-            Total_Deleted_Request:response.data.data.Total_Deleted_Request,
-            Total_Draft:response.data.data.Total_Draft,
-          });
-        },
-  
-        (error) => {}
-      );
-      }
+    axios.patch(url).then(
+      (response) => {
+        console.log("======alldata", response);
+      },
 
-    render() {
-        return (
-            <div>
-                   <RefreshIcon className="reload1" onClick={this.Reload}/>
-                <div className="allcard">
-                <div>
-                <Grid container>
-                <Grid item md={1}>
+      (error) => {}
+    );
+  };
+  componentDidMount = () => {
+    let url = "http://seo.srcservicesltd.com:8000" + "/count_statics";
 
-                    </Grid>
-                    <Grid item md={2}>
-<Card className="card1">
-<b>Total Registered Users</b>
-{/* <Bar
+    axios.get(url).then(
+      (response) => {
+        console.log("======", response);
+        this.setState({
+          id: response.data.id,
+          Veryfied_user: response.data.data.Veryfied_user,
+          Un_verified_user: response.data.data.Un_verified_user,
+          Total_User: response.data.data.Total_User,
+          Total_Resume: response.data.data.Total_Resume,
+          Total_Live: response.data.data.Total_Live,
+          Total_Deleted_Request: response.data.data.Total_Deleted_Request,
+          Total_Draft: response.data.data.Total_Draft,
+        });
+      },
+
+      (error) => {}
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <RefreshIcon className="reload1" onClick={this.Reload} />
+        <div className="allcard">
+          <div>
+            <Grid container>
+              <Grid item md={3}>
+                <Card className="card1">
+                  <b>Total Registered Users</b>
+                  {/* <Bar
           data={this.state.Total_User}
           options={{
             title:{
@@ -105,59 +94,88 @@ class Dashboard extends Component {
             }
           }}
         /> */}
-</Card>
-                    </Grid>
+                </Card>
+              </Grid>
 
-                    <Grid item md={2}>
-<Card className="middlecard">
-<b>Total Verified</b>
-<Circular value={this.state.Veryfied_user}  max={100} display="inline" withGrid={false} />
-</Card>
-                    </Grid>
-                    <Grid item md={2}>
-<Card className="card3 ">
-<b>Total Unverified Users</b>
+              <Grid item md={3}>
+                <Card className="middlecard">
+                  <b>Total Verified</b>
+                  <Circular
+                    value={this.state.Veryfied_user}
+                    max={100}
+                    display="inline"
+                    withGrid={false}
+                  />
+                </Card>
+              </Grid>
 
-<Circular value={this.state.Un_verified_user}  max={5} display="inline" withGrid={false} />
+              <Grid item md={6}>
+                <Card className="card3"></Card>
+              </Grid>
 
-</Card>
-                    </Grid>
-                </Grid>
-                </div>
-                <div>
+              {/* <Grid item md={3}>
+                <Card className="card3 ">
+                  <b>Total Unverified Users</b>
 
-                <Grid container>
-                <Grid item md={2}>
+                  <Circular
+                    value={this.state.Un_verified_user}
+                    max={5}
+                    display="inline"
+                    withGrid={false}
+                  />
+                </Card> */}
+              {/* </Grid> */}
+            </Grid>
+          </div>
+          <div>
+            <Grid container>
+              <Grid item md={3}>
+                <Card className="card1">
+                  <b>Total Draft</b>
+                </Card>
+              </Grid>
 
-                    </Grid>
-                    <Grid item md={2}>
-<Card className="cardr1">
-{/* <img className="imagedemo" src ="https://image.freepik.com/free-vector/business-abstract-infographics-with-3d-pie-info-char-graph-bar_53562-5873.jpg"/> */}
-<Histogram values={[47,100,32,45,67,89,32]} max={100} notAllreadyFull={true} withGrid={true} gridDivisionCount={10} display="" size={12} />
-<h1>Total Draft</h1>
-</Card>
-                    </Grid>
-                 
-                    <Grid item md={2}>
-<Card className="card3">
-<b>Total Resume</b>
+              <Grid item md={3}>
+                <Card className="card1">
+                  <b>Total Resume</b>
+                </Card>
+              </Grid>
 
-<Circular value={this.state.Total_Resume}  max={1000} display="inline" withGrid={false} />
-</Card>
-<Card className="card3">
-<b>Total Live Resume</b>
+              <Grid item md={6}>
+                <Card className="card3"></Card>
+              </Grid>
 
-<Circular value={this.state.Total_Live}  max={100} display="inline" withGrid={false} />
-</Card>
-                    </Grid>
-                </Grid>
+        
+            </Grid>
+          </div>
 
-                </div>
-               
-                </div>
-            </div>
-        );
-    }
+          <div>
+            <Grid container>
+              <Grid item md={3}>
+                <Card className="card1">
+                  <b>Total Unverified Users</b>
+                </Card>
+              </Grid>
+
+              <Grid item md={3}>
+                <Card className="card1">
+                  <b>Total Live Resume</b>
+                </Card>
+              </Grid>
+
+              {/* <Grid item md={6}>
+                <Card className="card3"></Card>
+              </Grid> */}
+
+        
+            </Grid>
+          </div>
+
+
+        </div>
+      </div>
+    );
+  }
 }
 
 export default HOC(Dashboard);
